c3e1f21386f5a4d7b205aef0133230b5fbcdfcf1
threepolesoft <threepolesoft@gmail.com>
2023-05-22 15:04:40 +0600

    init

---
+++ Darts/Models/TopTenItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Darts.Models
+{
+    public class TopTenItem
+    {
+        public string Title { get; set; }
+        public string Pic { get; set; }
+        public string Des { get; set; }
+        public bool IsVisible { get; set; } 
+    }
+}
